e402f9451ae11f247a01a1c084b487168c9acf41
ryo-murai <murai.ry@gmail.com>
2014-09-23 15:46:43 +0900

    chap 2.3 -- add a form / validation

--- app/controllers/Product.scala
+++ app/controllers/Product.scala
@@ -3,7 +3,7 @@ package controllers
 import play.api.data.Form
 import play.api.data.Forms.{mapping, longNumber, nonEmptyText}
 import play.api.i18n.Messages
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{Action, Controller, Flash}
 import models.Product
 
 object Products extends Controller {
@@ -17,6 +17,32 @@ object Products extends Controller {
     Product.findByEan(ean).map { product =>
       Ok(views.html.products.details(product))
     }.getOrElse(NotFound)
+  }
+
+  def save = Action { implicit request =>
+    val newProductForm = productForm.bindFromRequest()
+
+    newProductForm.fold(
+      hasErrors = { form =>
+        Redirect(routes.Products.newProduct())
+          .flashing(Flash(form.data) + ("error" -> Messages("validation.errors")))
+      },
+      success = { newProduct =>
+        Product.add(newProduct)
+        val message = Messages("products.new.success", newProduct.name)
+        Redirect(routes.Products.show(newProduct.ean))
+          .flashing("success" -> message)
+      }
+    )
+  }
+
+  def newProduct = Action { implicit request =>
+    val form = if (request2flash.get("error").isDefined)
+      productForm.bind(request2flash.data)
+    else
+      productForm
+
+    Ok(views.html.products.editProduct(form))
   }
 
   private val productForm: Form[Product] = Form (
